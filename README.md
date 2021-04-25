cf46d4772fc619d1d4ed0a688465d577babec182
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-24 20:43:40 -0400

    jumbotron files

--- README.md
+++
@@ -1,1 +0,0 @@
-# react_employee_directory
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div> className="container"
+            <h1>Employee Directory</h1>
+            <p>Find your employee via search or sort features below.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
