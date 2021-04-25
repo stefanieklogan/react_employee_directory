3b4f002f22f581b3a63096f4deb1936c146dbaa5
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-24 20:45:01 -0400

    searchBar files

---
+++ src/components/SearchBar/style.css
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar() {
+    return (
+        <div className="container">
+         <input type="text" className="search" placeholder="Search for employee"/>   
+        </div>
+    );
+}
+
+export default SearchBar;
