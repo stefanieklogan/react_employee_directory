ec9ab3b90650bde1dbfc430d8edf067cbbbfad04
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-27 12:40:52 -0400

    tableHtml maping

--- src/components/TableHtml/index.js
+++ src/components/TableHtml/index.js
@@ -1,30 +1,23 @@
-import React from "react";
-// import TableData from "../TableData"
-// import API from "../../utils"
-// var FontAwesome = require('react-fontawesome');
-// className="fas fa-sort-alpha-down">
+// import React, { Component } from 'react';
+import React from 'react';
+import "./style.css";
 
-import './style.css';
+function TableHtml (props)  {
 
-function TableHtml(props) {
-
-  const { format, headings, rows } = props;
+  let { format, headings, rows } = props;
 
   return (
-    <table className={`table ${props.format || "table-striped"}`}>
-      <thead>
-        <tr>
-          {headings.map(heading => <th scope="col">{heading}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-
+        <table className={`table ${format || "table-striped"}`}>
+        <thead>
+          <tr>
+            {headings.map(heading => <th scope="col">{heading}</th>)}
+          </tr>
+        </thead>
+        <tbody>      
           {rows.map(row => <tr>{row.map(item => <td>{item}</td>)}</tr>)}
-
-       
-      </tbody>
-    </table>
+        </tbody>
+      </table>
   );
-};
+}
 
 export default TableHtml;
