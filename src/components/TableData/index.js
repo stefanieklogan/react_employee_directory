f818867cb3c093c806e3baa621486a6b2c092907
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-27 12:42:39 -0400

    tableData mapping

--- src/components/TableData/index.js
+++ src/components/TableData/index.js
@@ -1,42 +1,56 @@
-import React from "react";
-import API from "../../utils/api";
+import React, { Component } from 'react';
+import TableHtml from '../TableHtml';
+import API from '../../utils/api'
 
 class TableData extends Component {
     state = {
-        result: {},
+        rows: [[]],
+        headings: ["Picture", "Name", "Email", "Phone", "Nationality"],
+        format: "",
         search: ""
     };
 
-componentDidMount() {
-    this.searchEmployee();
-}
+    componentDidMount() {
+        this.searchEmployee();
+    }
 
-searchEmployee = () => {
-    API.getEmployee()
-    .then(employee => {
-        console.log(employee);
-        this.setState({ result: employee.data })})
-        .catch(err => console.log(err));
-};
+    searchEmployee = () => {
+        API.getEmployee()
+            .then(employees => {
+                console.log(employees.data.results);
+                const rows = employees.data.results.map(employee => {
+                    console.log(employee);
+                    return [employee.picture.thumbnail, `${employee.name.first} ${employee.name.last}`, employee.email, employee.phone, employee.nat]
+                })
 
-handleInputChange = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-}
-}
-function TableData (props) {
-    return (
-        <div className="text-center">
-            <tr>
-                <th scope="row"></th>
-                <img alt={props.picture.thumbnail} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
-                <td>{props.name}</td>
-                <td>{props.email}</td>
-                <td>{props.phone}</td>
-                <td>{props.nat}</td>
-            </tr>
-        </div>
-    );
+                console.log(rows);
+                this.setState({ rows })
+            })
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        })
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchEmployee(this.state.search);
+    };
+
+    render() {
+        return (
+            <TableHtml
+                headings={this.state.headings}
+                rows={this.state.rows}
+                format={this.state.format}
+            />
+        )
+    };
 }
 
 export default TableData;
